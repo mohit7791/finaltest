f131897a30568b2391c9dd8b8241796685c2741b
Mohit <mohitwav@gmail.com>
2020-04-15 14:49:49 +0530

    test

--- backend/server.js
+++ backend/server.js
@@ -9,8 +9,8 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // API
-const users = require('/api/users');
-app.use('/api/users', users);
+const users = require('./api/users');
+app.use('./api/users', users);
 
 app.use(express.static(path.join(__dirname, '../build')))
 app.get('*', (req, res) => {
